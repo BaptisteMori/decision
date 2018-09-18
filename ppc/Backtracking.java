42f311e2a789ec09d5913d3209ef5bb6fa3997c1
21606807 <21606807@unicaen.fr>
2018-09-18 16:29:39 +0200

    imports package ppc

--- ppc/Backtracking.java
+++ ppc/Backtracking.java
@@ -1,8 +1,14 @@
 package ppc;
 
+import representations;
+import examples;
+
 public class Backtracking {
 
+  private String[Variable] variables;
+
   public Backtracking() {
-    
+
+
   }
 }
--- ppc/GenerateAndTest.java
+++ ppc/GenerateAndTest.java
@@ -1,8 +1,15 @@
 package ppc;
 
+import representations;
+import examples;
+
 public class GenerateAndTest {
 
+  private Variable[] variables;
+  private Constraint[] constraints;
+
   public GenerateAndTest() {
+
     
   }
 }
