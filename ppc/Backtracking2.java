ba30424285711591001d6c93c5c0ef1b7d59dd75
Chagneux Dimitri <21606807@n304l-401010.etu.info.unicaen.fr>
2018-09-19 17:57:11 +0200

    Backtracking2 (heuristic)

---
+++ ppc/Backtracking2.java
@@ -0,0 +1,122 @@
+package ppc;
+
+import representations.*;
+import examples.*;
+import java.util.*;
+
+public class Backtracking2 {
+
+  private Variable[] variables;
+  private Constraint[] constraints;
+  private Map<Variable,Integer> cpt = new HashMap<Variable,Integer>();
+
+  public Backtracking2(Variable[] variables, Constraint[] constraints) {
+    this.variables = variables;
+    this.constraints = constraints;
+  }
+
+  public void heuristic() {
+    Variable[] scopeTMP;
+    ArrayList<Variable> scope = new ArrayList<>();
+    for (Constraint c : constraints) {
+      scopeTMP = c.getScope().toArray(new Variable[c.getScope().size()]);
+      for (Variable v : scopeTMP) {
+        scope.add(v);
+      }
+    }
+    for (int i = 0; i< scope.size(); i++) {
+      int ncpt = 1;
+      for(int j = 0; j< scope.size(); j++) {
+        if (i != j) {
+          if (scope.get(i).equals(scope.get(j))) {
+            ncpt += 1;
+          }
+        }
+      }
+      if(!(this.cpt.containsKey(scope.get(i)))) {
+        this.cpt.put(scope.get(i),ncpt);
+      }
+    }
+  }
+
+  public Variable heuristicDomaine() {
+    ArrayList<Variable> vDom = new ArrayList<>();
+    int tmp = 0;
+    for (int v : this.cpt.values()) {
+      if (v > tmp) {
+        tmp = v;
+      }
+    }
+
+    for (Variable v : this.cpt.keySet().toArray(new Variable[this.cpt.size()])) {
+      if (this.cpt.get(v) == tmp) {
+        vDom.add(v);
+      }
+    }
+
+    tmp = 100;
+    Variable vMinDom = null;
+    for(Variable v : vDom) {
+      if (v.getDomaine().size() < tmp) {
+        tmp = v.getDomaine().size();
+        vMinDom = v;
+      }
+    }
+    return vMinDom;
+  }
+
+  public boolean backtrack(Map<Variable,String> voiture) {
+    boolean res = true;
+    int i=0;
+    Map<Variable,String> tmp = new HashMap<Variable,String>();
+    /*while (res) {
+      tmp = atomicAttribution(voiture, this.variables[i]);
+      i++;
+      res = allConstraintsSatisfiedBy(tmp);
+/*
+      if (res == false) {
+        tmp = voiture;
+        tmp = atomicAttribution(tmp, this.variables[i]);
+        res = true;
+      }
+      voiture = tmp;
+    }*/
+    return res;
+  }
+
+	public Map<Variable,String> atomicAttribution(Map<Variable,String> map, Variable v) {
+
+		Random r = new Random();
+		String[] domaine = v.getDomaine().toArray(new String[v.getDomaine().size()]);
+		String value = domaine[r.nextInt(domaine.length)];
+		map.put(v,value);
+		//System.out.println(map);
+		return map;
+	}
+
+	public boolean allConstraintsSatisfiedBy(Map<Variable,String> voiture) {
+		for (int i = 0; i< this.constraints.length; i++) {
+			if(!(this.constraints[i].isSatisfiedBy(voiture))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+  public Map<Variable,String> generateMap() {
+    Map<Variable,String> map = new HashMap<Variable,String>();
+    for (int i = 0; i < this.variables.length; i++) {
+      map.put(this.variables[i],"");
+    }
+    return map;
+  }
+
+	public Variable[] getVariables() {
+		return this.variables;
+	}
+
+	public Constraint[] getConstraints() {
+		return this.constraints;
+	}
+
+}
