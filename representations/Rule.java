64abeb161b3626cce99c9bf731460afd1bf86c2e
Chagneux Dimitri <21606807@n304l-401010.etu.info.unicaen.fr>
2018-09-26 18:11:01 +0200

    filter du rule qui avance

--- representations/Rule.java
+++ representations/Rule.java
@@ -47,29 +47,45 @@ public class Rule implements Constraint {
 	@Override
 	public boolean filter(Map<Variable,String> voiture, Map<Variable, Set<String>> unassigned_domains) {
 		boolean tmp = false;
-		if (this.isSatisfiedBy(voiture) && this.premisse != null) {
-			for (Variable v : this.conclusion.keySet()) {
-				if (unassigned_domains.size()==1 && voiture.get(v).equals("")) {
-          for (Variable va : this.conclusion.keySet()){
-            if(!(voiture.get(va).equals(""))) {
-              String value = voiture.get(va);
-              Set<String> dom = unassigned_domains.get(va);
+    /*Variable[] var_set = this.premisse.keySet().toArray(new Variable[this.premisse.keySet().size()]);
+    String couleur_var = "";
+    for (Variable v : var_set) {
+      if (v.getNom().equals("couleur_toit")) {
+        couleur_var = voiture.get(v);
+        System.out.println(couleur_var);
+      }
+    }*/
+
+    /*PAS FONCTIONNEL
+    if (this.isSatisfiedBy(voiture) && this.premisse != null) {
+      for (Variable v : this.conclusion.keySet()) {
+        if (!(voiture.get(v).equals(""))) {
+
+          Set<String> dom = unassigned_domains.get;
+          Set<String> tmp_dom = new HashSet<>();
+          tmp_dom.addAll(dom);
+          if (this.conclusion.get(v) == voiture.get(v)){
+
+          }*/
+
+          /*FONCTIONNEL MAIS BIZARRE (avec le premier commentaire)
+          for (Variable v2 : this.conclusion.keySet()) {
+            if (!(v.equals(v2))) {
+              Set<String> dom = unassigned_domains.get(v2);
               Set<String> tmp_dom = new HashSet<>();
               tmp_dom.addAll(dom);
-              for (String s : tmp_dom){
-                if (dom.contains(s) && s == value){
+              for (String s : tmp_dom) {
+                if(!(voiture.get(v).equals(couleur_var)) && s != couleur_var) {
                   dom.remove(s);
                   tmp = true;
                 }
               }
-              unassigned_domains.put(v,dom);
+              unassigned_domains.put(v2,dom);
             }
           }
-          //voiture.put(v,this.conclusion.get(v));
-					//return tmp;
-				}
-			}
-		}
-		return tmp;
-	}
+        }
+      }
+    }*/
+    return tmp;
+  }
 }
