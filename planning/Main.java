b47ec7c80910c38957ed9f379a68ed636d9d0986
Elsion14 <dchagneux@live.fr>
2018-10-16 13:04:20 +0200

    modifications main

--- planning/Main.java
+++ planning/Main.java
@@ -8,16 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		AssemblyLine universe = new AssemblyLine();
-		System.out.println(universe.getAllActions().get(0).is_applicable(universe.getInitialState()));
 		State goalState = universe.generateGoalState();
 		ArrayList<State> goals = new ArrayList<>();
 		goals.add(goalState);
-		ArrayList<Action> actions = new ArrayList<>();
-		//actions.addAll(universe.getAllActions());
-		PlanningProblem pb = new PlanningProblem(universe.getInitialState(), goals, actions );
+		PlanningProblem pb = new PlanningProblem(universe.getInitialState(), goals, universe.getAllActions() );
 		Stack<Action> plan = new Stack<>();
 		ArrayList<State> closed = new ArrayList<>();
-		Stack<Action> actions_dfs = pb.dfs(universe.getInitialState(), plan, closed); //OK
+		Stack<Action> actions_dfs = pb.dfs(universe.getInitialState(), plan, closed);
 		System.out.println("\nDFS: "+actions_dfs);
 		Stack<Action> actions_bfs = pb.bfs();
 		System.out.println("\nBFS: "+actions_bfs);
