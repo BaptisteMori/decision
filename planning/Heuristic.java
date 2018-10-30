f9d3e236fed71ee7404bb9580fa3536fd76ea6ec
nicolaks <nico145702@gmail.com>
2018-10-30 14:30:14 +0100

    Ajout de Javadoc pour Heuristic.java

--- planning/Heuristic.java
+++ planning/Heuristic.java
@@ -2,5 +2,10 @@ package planning;
 
 public interface Heuristic {
 
+	/**
+		* Constructeur de l'interface
+		* @param s , qui est un State.
+		* @param goal , qui est un State.
+		*/
 	public double heuristic(State s, State goal);
 }
