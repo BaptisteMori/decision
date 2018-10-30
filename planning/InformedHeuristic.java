7edea8317d00376710fe7cb7de66afaa4916d071
nicolaks <nico145702@gmail.com>
2018-10-30 14:32:55 +0100

    Ajout de Javadoc pour informedHeuristic.java

--- planning/InformedHeuristic.java
+++ planning/InformedHeuristic.java
@@ -4,6 +4,13 @@ import representations.*;
 
 public class InformedHeuristic implements Heuristic {
 
+
+	/**
+		*  Redéfinition du constructeur de l'interface Heuristic.
+		* @param s , qui est un State.
+		* @param goal , qui est un State.
+		* @return heuristic_value , qui est un double et qui sera l'heuristic prédit.
+		*/
 	@Override
 	public double heuristic(State s, State goal) {
 		double heuristic_value = 0.;
