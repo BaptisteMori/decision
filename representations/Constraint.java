82f232bc748ceff442cce2c812a84cf6b116c7b5
nicolaks <nico145702@gmail.com>
2018-10-31 18:18:56 +0100

    Ajout de Javadoc pour Constraint.java

--- representations/Constraint.java
+++ representations/Constraint.java
@@ -6,23 +6,23 @@ public interface Constraint {
 
 
   /**
-    *
+    * Méthode permettant de retourner le scope.
     *
     *
     */
   public Set<Variable> getScope();
 
   /**
-    *
-    *
+    * Méthode permettant de savoir si une contrainte est satisfaite.
+    * @param map , qui est un Map de Variable et de String.
     *
     */
   public boolean isSatisfiedBy(Map<Variable,String> map);
 
   /**
-    *
-    *
-    *
+    * Méthode qui utilise les filtres afin de réduire les recherches.
+    * @param map , qui est un Map de Variable et de String.
+    * @param domaines , qui est un Map de Variable et de Set de String.
     */
   public boolean filter(Map<Variable,String> map, Map<Variable, Set<String>> domaines);
 }
