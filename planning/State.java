630c2d795736243f379bf1db94576ff880dc26f4
nicolaks <nico145702@gmail.com>
2018-10-31 15:36:53 +0100

    Ajout de Javadoc pour State.java

--- planning/State.java
+++ planning/State.java
@@ -7,6 +7,11 @@ public class State {
 
   private Map<Variable,String> state;
 
+  /**
+		* Constructeur de la classe State.
+		* @param state , qui est un Map de Varaible et de String.
+		*
+		*/
   public State(Map<Variable,String> state) {
     this.state = state;
   }
@@ -15,6 +20,12 @@ public class State {
 		this(null);
 	}
 
+
+  /**
+		* Surcharge de la méthode hashCode().
+		* @return code , qui est un int et qui contiendra le hash.
+		*
+		*/
   @Override
 	public int hashCode() {
 		int code=7;
@@ -22,6 +33,12 @@ public class State {
 		return code;
 	}
 
+
+  /**
+		* Surcharge de la méthode equals(), qui permet de vérifier si deux objets sont égaux.
+		* @param o , qui est un Object.
+		* @return true , si les deux objets sont égaux, sinon retourne false.
+		*/
   @Override
   public boolean equals(Object o) {
     if (o==this) {
@@ -38,14 +55,31 @@ public class State {
     return true;
   }
 
+  /**
+		* Méthode permettant de retourner le State.
+		* @return this.state , qui est un Map de Variable et de String.
+		*
+		*/
   public Map<Variable,String> getState() {
     return this.state;
   }
 
+
+  /**
+		* Méthode permettant de mettre à jour un State.
+		* @param state , qui est un Map de Variable et de String.
+		*
+		*/
   public void setState(Map<Variable,String> state) {
     this.state = state;
   }
 
+
+  /**
+		* Méthode permettant d'afficher le State.
+		* @return this.state.toString() , qui sera de type String.
+		*
+		*/
 	public String toString() {
 		return this.state.toString();
 	}
