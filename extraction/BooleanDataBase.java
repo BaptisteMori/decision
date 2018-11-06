8253def2817a5400afaf074e459eae61cc03e49a
Elsion14 <dchagneux@live.fr>
2018-11-06 17:07:51 +0100

    voilà

--- extraction/BooleanDataBase.java
+++ extraction/BooleanDataBase.java
@@ -1,4 +1,15 @@
 package extraction;
 
+import java.util.*;
+import representations.*;
+
 public class BooleanDataBase {
+
+  private ArrayList<Variable> variables_list;
+  private ArrayList<Map<Variable,String>> transactions;
+
+  public BooleanDataBase() {
+    this.variables_list = new ArrayList<Variable>();
+    this.transactions = new ArrayList<Map<Variable,String>>();
+  }
 }
