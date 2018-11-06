0364ed925b6deefbc2782b0901abd1b668c942bc
Elsion14 <dchagneux@live.fr>
2018-11-06 17:12:47 +0100

    début classe FrequentItemSetMiner

---
+++ extraction/FrequentItemSetMiner.java
@@ -0,0 +1,16 @@
+package extraction;
+
+import representations.*;
+
+public class FrequentItemsetMiner {
+
+  private BooleanDataBase boolean_database;
+
+  public FrequentItemSetMiner() {
+
+  }
+
+  public Map<Set<Variable>,Integer> frequentItemSets (Integer minimal_support) {
+    return null;
+  }
+}
