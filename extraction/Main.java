494085e9bf7e604ca9719d07d833561fbbec2b3b
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-11-06 17:13:36 +0100

    main

---
+++ extraction/Main.java
@@ -0,0 +1,12 @@
+package extraction;
+
+import java.util.*;
+import representations.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BooleanDataBase data = new BooleanDataBase();
+		FrequentItemsetMiner naive_miner = new FrequentItemsetMiner();
+	}
+}
