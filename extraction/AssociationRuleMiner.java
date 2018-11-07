92b61bb9eb034bbba1a4ebc5ce916dec6ff2d871
Elsion14 <dchagneux@live.fr>
2018-11-07 16:49:23 +0100

    Début AssociationRuleMiner

---
+++ extraction/AssociationRuleMiner.java
@@ -0,0 +1,17 @@
+package extraction;
+
+import java.util.*;
+
+public class AssociationRuleMiner {
+
+  private Map<Set<String>,Integer> frequent_item_sets;
+
+  public AssociationRuleMiner(Map<Set<String>,Integer> frequent_item_sets) {
+    this.frequent_item_sets = frequent_item_sets;
+  }
+
+  public int frequentValidAssociation(int minfr, int minconf) {
+
+    return 0;
+  }
+}
