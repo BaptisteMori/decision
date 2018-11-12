87b36ae2fd9752b4bb8fef00f36f4de8be59b543
Aubry Nicolas <21603763@C304L-409C06.campus.unicaen.fr>
2018-11-12 12:17:39 +0100

    Mis à jour de la Javadoc

--- planning/Heuristic.java
+++ planning/Heuristic.java
@@ -2,10 +2,6 @@ package planning;
 
 public interface Heuristic {
 
-	/**
-		* Constructeur de l'interface
-		* @param s , qui est un State.
-		* @param goal , qui est un State.
-		*/
+
 	public double heuristic(State s, State goal);
 }
