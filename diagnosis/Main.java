d5aea669a2adc88198040e81d3c59b1b89bbe678
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-11-13 17:13:27 +0100

    main bien

--- diagnosis/Main.java
+++ diagnosis/Main.java
@@ -1,12 +1,15 @@
 package diagnosis;
 
+import representations.*;
 import examples.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Variable> vars = new ArrayList<>(Arrays.asList(ex.getVariables()));
-		ArrayList<Constraint> constraints = new ArrayList<>(Arrays.asList(ex.getVariables()));
+		Example ex = new Example();
+		ArrayList<Variable> vars = new ArrayList<Variable>(Arrays.asList(ex.getVariables()));
+		ArrayList<Constraint> constraints = new ArrayList<Constraint>(Arrays.asList(ex.getConstraints()));
 		Diagnoser diag = new Diagnoser(vars, constraints);
 	}
 }
