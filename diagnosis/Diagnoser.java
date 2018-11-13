23735403fa87356fb7fb3670a292e8d9a408b27d
Elsion14 <dchagneux@live.fr>
2018-11-13 16:56:45 +0100

    Petites avancées Diagnoser

--- diagnosis/Diagnoser.java
+++ diagnosis/Diagnoser.java
@@ -1,4 +1,21 @@
 package diagnosis;
 
+import representations.*;
+
 public class Diagnoser {
+
+  private ArrayList<Variable> variables;
+  private ArrayList<Constraint> contraintes;
+  private Map<Variable,String> current_instance;
+
+  public Diagnoser(ArrayList<Variable> variables, ArrayList<Constraint> contraintes) {
+    this.variables = variables;
+    this.contraintes = contraintes;
+    this.current_instance =  = new HashMap<Variable,String>();
+  }
+
+  public boolean isIncompatible(Map<Variable,String> incomplete_instance, Variable variable, String value) {
+    
+    return true;
+  }
 }
