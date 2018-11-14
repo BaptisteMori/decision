f3185b91a2185cbd6e689264a0131d2d2e1b4a62
nicolaks <nico145702@gmail.com>
2018-11-14 17:13:45 +0100

    Ajout de Javadoc pour :       - DataBase.java       - FrequentItemSetMiner.java

--- extraction/FrequentItemSetMiner.java
+++ extraction/FrequentItemSetMiner.java
@@ -8,14 +8,33 @@ public class FrequentItemSetMiner {
   private BooleanDataBase boolean_database;
   private Map<Set<Variable>, Integer> frequentItemSets;
 
+
+/**
+  * Constructeur de la Classe.
+  * @param db , qui est de type BooleanDataBase.
+  *
+  */
+
   public FrequentItemSetMiner(BooleanDataBase db) {
 		this.boolean_database=db;
     this.frequentItemSets = new HashMap<Set<Variable>, Integer>();
   }
 
+/**
+  * Méthode qui permet de retourner l'item d'un Set.
+  * @return this.frequentItemSets , retournant un Map de Set de Variable et un integer.
+  *
+  */
 	public Map<Set<Variable>, Integer> getItemSets() {
 		return this.frequentItemSets;
 	}
+
+
+/**
+  *
+  * @param minimal_support , qui est de type int.
+  * @param combi , qui est de type Set de Variable.
+  */
 
 	public void bfMiner(int minimal_support, Set<Variable> combi) {
 		int f = frequencyCalcul(combi);
@@ -32,6 +51,12 @@ public class FrequentItemSetMiner {
 		}
 	}
 
+
+  /**
+    * Méthode permettant d'afficher les Set d'item fréquent.
+    * @param minimal_support , qui est un int.
+    * @param prev , qui est un Set de Variable.
+    */
   public Map<Set<Variable>,Integer> frequentItemSets (int minimal_support, Set<Variable> prev) {
     Set<Variable> item_set = new HashSet<Variable>();
     if (frequencyCalcul(item_set) >= minimal_support) {
@@ -41,6 +66,12 @@ public class FrequentItemSetMiner {
     return null;
   }
 
+
+  /**
+    * Méthode
+    * @param combi, qui est un Set de Variable.
+    * @return this.boolean_database.getTransactions().size() , qui est un entier représentant la fréquence de calcul.
+    */
   public int frequencyCalcul(Set<Variable> combi) {
     if (combi.size() == 0) {
       return this.boolean_database.getTransactions().size();
