5f1304eda9ec3037a8efaf2601d2fdca38c0e029
Elsion14 <dchagneux@live.fr>
2018-11-14 18:12:14 +0100

    Diagnoser avance mais pas bon

--- diagnosis/Diagnoser.java
+++ diagnosis/Diagnoser.java
@@ -9,43 +9,51 @@ public class Diagnoser {
   private ArrayList<Variable> variables;
   private ArrayList<Constraint> contraintes;
   private Map<Variable,String> current_instance;
+  private Backtracking bt;
+
 
   public Diagnoser(ArrayList<Variable> variables, ArrayList<Constraint> contraintes) {
     this.variables = variables;
     this.contraintes = contraintes;
     this.current_instance = new HashMap<Variable,String>();
+    this.bt = new Backtracking(this.variables.toArray(new Variable[this.variables.size()]), this.contraintes.toArray(new Constraint[contraintes.size()]));
   }
 
-  public boolean isExplication(Map<Variable,String> instance, Variable variable, String value) {
-    ArrayList<Variable> variables_bis = new ArrayList<Variable>();
-    variables_bis.addAll(instance.keySet());
-    for (Variable var : variables_bis) {
-      var.setDomaine(new HashSet<String>(Arrays.asList(value)));
+  public void add(Variable var, String value){
+    this.current_instance.put(var,value);
+  }
+
+  public boolean isExplanation(Map<Variable,String> instance, Variable variable, String value) {
+    Map<Variable,String> instance_bis;
+    instance_bis = new HashMap<>(instance);
+    for (Variable var : instance_bis.keySet()) {
+      var.setDomaine(new HashSet<String>(Arrays.asList(instance_bis.get(var))));
     }
     variable.setDomaine(new HashSet<String>(Arrays.asList(value)));
-    Backtracking backtracking = new Backtracking(variables_bis.toArray(new Variable[variables_bis.size()]),contraintes.toArray(new Constraint[contraintes.size()]));
-    backtracking.backtrack(instance,0);
-    return backtracking.getList().isEmpty();
+
+    instance_bis.put(variable,value);
+    this.bt.backtrack(instance_bis,0);
+    System.out.println("isExplanation  "+this.bt.getList().isEmpty());
+    return this.bt.getList().isEmpty();
   }
 
-  public Map<Variable,String> getDiagnosis(Map<Variable,String> instance, Variable variable, String value) {
-    Map<Variable,String> f = new HashMap<Variable,String>();
-    f.putAll(this.current_instance);
-    Map<Variable,String> f_bis = new HashMap<Variable,String>();
-    f_bis.putAll(f);
-    Map<Variable,String> current_copy = new HashMap<Variable,String>();
-    current_copy.putAll(this.current_instance);
-    Map<Variable,String> instance_bis = new HashMap<Variable,String>();
-    while (!(f_bis.isEmpty())){
-      for (Variable var : f.keySet()) {
-        instance_bis.putAll(current_copy);
-        instance_bis.remove(var);
-        if(this.isExplication(instance_bis,variable,value)){
-          current_copy = instance_bis;
-        }
-        f_bis.remove(var);
+  public Map<Variable,String> getDiagnosis(Variable variable, String value) {
+    Map<Variable,String> res = new HashMap<Variable,String>();
+    res.putAll(this.current_instance);
+    Map<Variable,String> choices = new HashMap<Variable,String>();
+    choices.putAll(this.current_instance);
+    Map<Variable,String> res_bis;
+
+    for (Variable var : choices.keySet()) {
+      res_bis = new HashMap(res);
+      res_bis.remove(var);
+      System.out.println("\n\nvar = "+var);
+      System.out.println("e = "+res);
+      System.out.println("ePrime = "+res_bis);
+      if (this.isExplanation(res_bis,variable,value)) {
+        res = new HashMap<>(res_bis);
       }
     }
-    return current_copy;
+    return res;
   }
 }
