1a64a7c8b54f0f50579782a35dae5ffd3efd3bc6
Chagneux Dimitri <21606807@n304l-401010.etu.info.unicaen.fr>
2018-11-29 10:23:38 +0100

    Main package extraction

--- extraction/Main.java
+++ extraction/Main.java
@@ -12,9 +12,10 @@ public class Main {
 		DBReader csv_reader = new DBReader(new HashSet<Variable>(Arrays.asList(ex.getVariables())));
 		DataBase db = csv_reader.importDB("resources/example_db.csv");
 		BooleanDataBase bool_db = db.propositionalisation();
-    FrequentItemSetMiner miner = new FrequentItemSetMiner(bool_db);
-    miner.generateSingletons(3500);
-    miner.bfMiner(1,3500,new HashSet<Variable>());
+    FrequentItemSetMiner miner = new FrequentItemSetMiner(bool_db,3500);
+    miner.frequentItemSets();
     System.out.println(miner.getItemSets());
+		AssociationRuleMiner rule_miner = new AssociationRuleMiner(miner.getItemSets());
+		System.out.println(rule_miner.frequentValidAssociation(0.4f));
 	}
 }
